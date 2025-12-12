5d3701206d9145136ef0ff7d81db43e7383670e9
Anjali sharma <anjalisharma1562005@gmail.com>
2025-12-13 02:07:06 +0530

    updated fileuploadfile

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,9 +1,37 @@
-"use client"
-import React from 'react'
+"use client" 
+import { useDropzone } from 'react-dropzone'
+import { useCallback } from 'react'
+import { CircleArrowDown, RocketIcon } from 'lucide-react'
+
 
 function FileUploader() {
+     const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  
+  
   return (
-    <div>FileUploader</div>
+    
+     <div {...getRootProps()}
+     className='p-2 border-2 border-dashed w-[90%]'
+     >
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+        (<>
+        <RocketIcon     className='h-20 w-20 animate-ping'/>
+         <p>Drop the files here ...</p>
+        </>)
+          :
+         ( 
+         <>
+         <CircleArrowDown className='animate-bounce h-20 w-20'/>
+         <p>Drag n drop some files here, or click to select files</p>
+            </>
+         )
+      }
+    </div>
   )
 }
 
